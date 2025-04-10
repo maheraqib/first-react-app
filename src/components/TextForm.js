93ff668571bb087646d3e78a0d04fea1ac9a7be2
maheraqib <maheraqibofficial@gmail.com>
2025-04-10 20:54:01 +0500

    add word counter functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,13 +18,32 @@ export default function TextForm(props) {
   const [text, setText] = useState("Enter text here");
 
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+    <>
+      <div className="container my-3">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            onChange={handleOnChange}
+            value={text}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Change To UpperCase
+        </button>
       </div>
-      <button className='btn btn-primary' onClick={handleUpClick}> Change To UpperCase</button>
 
-    </div>
+      <div className="container my-2">
+        <h2> Your Text Summery</h2>
+        <p>
+          {text.trim().length === 0 ? 0 : text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>
+          {0.008 * text.split(" ").length} Minutes is required to read
+        </p>
+      </div>
+    </>
   );
 }
